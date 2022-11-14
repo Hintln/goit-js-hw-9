7195e87e344fa8087a92a2aac70d1ce7592a64a9
Hintln <andrejlityprog@gmail.com>
2022-11-14 22:10:19 +0200

    start (3)

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,14 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const delayInput = document.querySelector("[name='delay']");
+const stepInput = document.querySelector("[name='step']");
+const amountInput = document.querySelector("[name='amount']");
+const createPromiseBtn = document.querySelector("[type='submit]");
+
+createPromiseBtn.addEventListener('click', submitBtnClick);
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
