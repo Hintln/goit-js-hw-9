49722e0a67520b931c6616dfa1e6c1092986c4b0
Hintln <andrejlityprog@gmail.com>
2022-11-14 11:42:38 +0200

    add..

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,3 +2,10 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import { Report } from 'notiflix/build/notiflix-report-aio';
 
+const startBtm = document.querySelector('[data-start]');
+const dayRefs = document.querySelector('[data-days]');
+const houerRefs = document.querySelector('[data-hours]');
+const minuteRefs = document.querySelector('[data-munites]');
+const secondRefs = document.querySelector('[data-seconds]');
+let timerId = null;
+
