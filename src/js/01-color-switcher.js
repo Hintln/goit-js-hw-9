5addac909d87c4298d7361df16284fe0b4622d84
Hintln <andrejlityprog@gmail.com>
2022-11-14 10:31:52 +0200

    .

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,17 +1,9 @@
 const startBtm = document.querySelector('[data-start]');
 const stopBtm = document.querySelector('[data-stop]');
-
-
-
-
-
-
-
-
-
-
+let timerId = null;
 
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
