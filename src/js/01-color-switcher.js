e41f2f49b1d2362fcf7cbc9fcfbc223211936e47
Hintln <andrejlityprog@gmail.com>
2022-11-14 10:50:18 +0200

    done(1+)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,7 +3,24 @@ const stopBtm = document.querySelector('[data-stop]');
 let timerId = null;
 
 
+
+
+startBtm.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+        document.body.style.backgroundColor = color;
+    }, 1000);
+    startBtm.disabled = true;
+    stopBtm.disabled = false;
+});
+
+stopBtm.addEventListener('click', () => {
+    clearInterval(timerId);
+    startBtm.disabled = false;
+    stopBtm.disabled = true;
+});
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
